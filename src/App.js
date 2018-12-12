ea9ca28399e0fbf05cd1f8fc3106f2dbf2319553
Ed Putans <edputans@gmail.com>
2018-12-12 13:49:48 +0000

    created basic welcome page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { BrowserRouter,withRouter, Route, Switch, Redirect } from 'react-router-dom'
+
+import Login from './containers/Login'
+import Welcome from './containers/Welcome'
+import Signup from './containers/Signup'
 import './App.css';
 import * as adapter from './Adapter'
-import { BrowserRouter,withRouter, Route, Switch, Redirect } from 'react-router-dom'
 
 class App extends Component {
 
@@ -10,15 +14,16 @@ class App extends Component {
   }
 
 
-
   render() {
     return (
-      
         <div>
-        
+            <Switch>
+              {/* because user is signed out, we currently only work on these ;p */}
+                <Route path='/login' component={ props => <Login {...props} /> } />
+                <Route path='/signup' component={ props => <Signup { ...props } /> } />
+                <Route exact path='/' component={ props => <Welcome { ...props } /> } />
+            </Switch>
         </div>
-     
-      
     )
   }
 }
--- src/containers/Welcome.js
+++ src/containers/Welcome.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Button} from 'semantic-ui-react'
 
 export default class Welcome extends React.Component {
 
     render() {
-        return (<div></div>)
+        return (
+            <div>
+                <div onClick = {() => this.props.history.push('/login')}>
+                    <Button color='yellow'>Log In</Button>
+                </div>
+                <div onClick={ () => this.props.history.push('/signup')}>
+                    <Button basic color='blue'>Sign up</Button>
+                </div>
+            </div>
+        )
     }
-}
+}                    
