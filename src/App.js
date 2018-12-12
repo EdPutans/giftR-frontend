8d5552f814d6146cfaa5ef64f487ac67214d47a5
Ed Putans <edputans@gmail.com>
2018-12-12 13:16:11 +0000

    set up and troubleshot react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 import * as adapter from './Adapter'
+import { BrowserRouter,withRouter, Route, Switch, Redirect } from 'react-router-dom'
 
 class App extends Component {
+
+  state = {
+    currentUser: null,
+  }
+
+
+
   render() {
     return (
-      <div className="App">
-          {  }
-      </div>
-    );
+      
+        <div>
+        
+        </div>
+     
+      
+    )
   }
 }
 
