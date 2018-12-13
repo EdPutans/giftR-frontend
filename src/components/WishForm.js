40dbe676580e70a3840cefdb3263836d05743d98
Ed Putans <edputans@gmail.com>
2018-12-13 15:03:44 +0000

    styling adjustment for forms

--- src/components/WishForm.js
+++ src/components/WishForm.js
@@ -1,54 +1,82 @@
 import React from 'react'
-import {Form, Button, Radio, Icon} from 'semantic-ui-react'
+import { Form, Button, Radio, Icon, Label } from 'semantic-ui-react'
 
 export default class WishForm extends React.Component {
 
-    state={
-        stars: 3
+    state = {
+        rating: 0
     }
-    
+
     selection = [
-        { value: 1, label: "★"},
-        { value: 2, label: "★★"},
-        { value: 3, label: "★★★"},
-        { value: 4, label: "★★★★"},
+        { value: 1, label: "★" },
+        { value: 2, label: "★★" },
+        { value: 3, label: "★★★" },
+        { value: 4, label: "★★★★" },
         { value: 5, label: "★★★★★" }
     ]
 
-    setStars = (num) => this.setState({stars: parseInt(num)})
+    setStars = (num) => this.setState({ rating: parseInt(num) })
 
     render() {
         return (<div>
             <Form>
                 <Form.Field>
-                    <input type="text" name="name" placeholder="Title"></input>
+                    <input 
+                        type="text" 
+                        name="name" 
+                        placeholder="Title" 
+                        onChange={event=>this.setState({name: event.target.value})}
+                        />
                 </Form.Field>
                 <Form.Field>
-                    <input type="text" name="description" placeholder="Description"></input>
-                </Form.Field>
-                <Form.Field>
-                    <input type="text" name="url" placeholder="Purchase link"></input>
-                </Form.Field>
-                <Form.Field>
-                    <input type="text" name="img_url" placeholder="Picture URL"></input>
-                </Form.Field>
-                <Form.Field>
-                    <input type="number" name="price" placeholder="Price"></input>
-                </Form.Field>
-                 <label> Priority: </label>
-                <Form.Group>
-                    {this.selection.map(s => 
-                    <Form.Field
-                        key={s.value}
-                        control={ Radio }
-                        label={s.label}
-                        value={s.value}
-                        checked={ this.state.stars === s.value }
-                        onChange={e=> this.setStars(s.value) }
+                    <input
+                        type="text"
+                        name="description"
+                        placeholder="Description"
+                        onChange={ event => this.setState({ description: event.target.value }) }
                     />
-                    ) }
-                </Form.Group>
-                
+                </Form.Field>
+                <Form.Field>
+                    <input
+                        type="text"
+                        name="url"
+                        placeholder="Link to purchase"
+                        onChange={ event => this.setState({ url: event.target.value }) }
+                    />                
+                </Form.Field>
+                <Form.Field>
+                    <input
+                        type="text"
+                        name="img_url"
+                        placeholder="Image link"
+                        onChange={ event => this.setState({ img_url: event.target.value }) }
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <input
+                        maxLength="2"
+                        type="number"
+                        name="price"
+                        placeholder="Price"
+                        onChange={ event => this.setState({ price: parseInt(event.target.value) }) }
+                    />
+                </Form.Field>
+                <Label > Priority: <div>
+                    <Form.Group inline>
+                        { this.selection.map(s =>
+                            <Form.Field
+                                key={ s.value }
+                                control={ Radio }
+                                label={ s.label }
+                                value={ s.value }
+                                checked={ this.state.stars === s.value }
+                                onChange={ () => this.setStars(s.value) }
+                            />
+                        ) }
+                    </Form.Group>
+                </div>
+                </Label>
+
             </Form><br />
             <Button>Make a wish!</Button>
         </div>)
--- src/containers/Wishlist.js
+++ src/containers/Wishlist.js
@@ -4,11 +4,28 @@ import { Button, Icon } from 'semantic-ui-react'
 
 export default class Wishlist extends React.Component {
 
+
+    handleClick =() => this.props.history.push('/new_wish')
     render() {
         return (
-        <div>
-            {this.props.wishes.map(w => <Wish key={w.id} wish={w} user={this.props.currentUser}/> )  }
-                <Button floated="right" style={ { position: 'absolute', right: '2em', bottom: '2em',textAlign: 'center' } }><Icon name="add"/></Button>
-        </div>)
+            <div>
+                { this.props.wishes.map(w => <Wish key={ w.id } wish={ w } user={ this.props.currentUser } />) }
+                <div>
+                        <Button
+                            className="circular"
+                            color="teal"
+                            onClick={this.handleClick}
+                            floated="right"
+                            style={ {
+                                position: 'absolute',
+                                right: '2em',
+                                bottom: '2em',
+                                textAlign: 'center'
+                            } }
+                        >
+                            <h1>+</h1>
+                        </Button>
+                </div>
+            </div>)
     }
 }
