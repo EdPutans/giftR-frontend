0449a9ba80e1f8625571f2f40334b554f55af571
Edgar <edputans@gmail.com>
2018-12-31 14:45:15 +0000

    now correctly renders SS but no notifications yet, styling and animations are also to be added

--- src/containers/SantaMain.js
+++ src/containers/SantaMain.js
@@ -8,57 +8,52 @@ export default class SantaMain extends React.Component {
     state = {
         randomized: [],
         ids: [],
-        users:[]
+        users: []
     }
 
 
     addUser = (value) => {
-        !this.state.users.includes(value) && this.setState({users: [...this.state.users, value]})
+        !this.state.users.includes(value) && this.setState({ users: [...this.state.users, value] })
         !this.state.ids.includes(value.id) &&
             this.setState({ ids: [...this.state.ids, value.id] })
     }
 
 
-    mapIdsForRandomizer=(objectArray)=>{
-        return objectArray.map(o=>o.id)
+    mapIdsForRandomizer = (objectArray) => {
+        return objectArray.map(o => o.id)
     }
 
     randomizer = (passedArray) => {
-        if(passedArray.length<2) {return;}
+        if (passedArray.length < 2) { return; }
         let array = [...passedArray]
         let gifters = [...passedArray]
-        
-        
-        let receivers = array.sort(e=>0.5-Math.random())
-        console.log({receivers, gifters})
-        
-        if(receivers.find(id => receivers.indexOf(id) === gifters.indexOf(id)))
-        {
-            this.randomizer(passedArray)
-        }else{
-                let result = receivers.map(id => ({ 
-                    receiver_id:id, 
-                    gifter_id: gifters[receivers.indexOf(id)] 
-                }))
-                this.setState({randomized: result})
-                console.log(result)
-                let mappedPeople = this.mapRandomizedToPeople(result)
-                this.setState({mappedPeople})
-                return result
-            }
-        }
-    
 
-        mapRandomizedToPeople = (arg) =>{
-            let mapper = arg.map(obj => {
-                let receiver = this.state.users.find(u => u.id === obj.receiver_id)
-                let gifter = this.state.users.find(u => u.id === obj.gifter_id)
-                console.log('mapped:',{receiver, gifter})
-                return ({gifter, receiver})
-            })
-            console.log('mapped pepel: ', mapper)
-            return mapper
+
+        let receivers = array.sort(e => 0.5 - Math.random())
+
+        if (receivers.find(id => receivers.indexOf(id) === gifters.indexOf(id))) {
+            this.randomizer(passedArray)
+        } else {
+            let result = receivers.map(id => ({
+                receiver_id: id,
+                gifter_id: gifters[receivers.indexOf(id)]
+            }))
+            this.setState({ randomized: result })
+            let mappedPeople = this.mapRandomizedToPeople(result)
+            this.setState({ mappedPeople })
+            return result
         }
+    }
+
+
+    mapRandomizedToPeople = (arg) => {
+        let mapper = arg.map(obj => {
+            let receiver = this.state.users.find(u => u.id === obj.receiver_id)
+            let gifter = this.state.users.find(u => u.id === obj.gifter_id)
+            return ({ gifter, receiver })
+        })
+        return mapper
+    }
 
 
     render() {
@@ -71,35 +66,33 @@ export default class SantaMain extends React.Component {
             }}>
                 <Header title={'Secret Santa'} />
 
-                <Grid columns={2}>
+                <Grid columns={2} >
                     <Grid.Row>
                         <Grid.Column>
-                            
-                            <div style={{margin: '0 3em 0 3em'}}>
-                            Add people to the randomizer here:
+
+                            <div style={{ margin: '0 3em 0 3em' }}>
+                            <div><input placeholder='budget' onChange={event => this.setState({budget: parseInt(event.target.value)})} /></div>
+                                Add people to the randomizer here:
                                 <AutosuggestForm
                                     addUser={this.addUser}
                                 />
                             </div>
+                            {this.state.ids.length > 0 && <div>
+                                <h4>Selected users:</h4>
+                                {this.state.users.map(e => <div>{e.first_name} {e.last_name}<br /></div>)}
+                            </div>}
+
                         </Grid.Column>
                         <Grid.Column>
-                            {this.state.ids.length > 0 && <div>
-                                 <h4>Selected users:</h4>
-                                <div>
-                                    {this.state.users.map(e => e.id).join(',')}
-                                    <br /> V <br />
-                                    {this.state.randomized.length > 0 && this.state.randomized.map(e=>e.receiver_id).join(',')}<br />
-                                    {
-                                        this.state.mappedPeople && this.state.mappedPeople.map(u => 
-                                            <div>{u.gifter.first_name} to {u.receiver.first_name} </div>
-                                        )
+                            
+                            {this.state.users.length < 2 ? "Please add at least 2 users!" : <Button onClick={e => this.randomizer(this.mapIdsForRandomizer(this.state.users))}>Randomize</Button> }
 
-                                    }
+                            {
+                                this.state.mappedPeople && this.state.mappedPeople.map(u =>
+                                    <div style={{ textAlign: 'center' }}>{u.gifter.first_name} {u.gifter.last_name} ---> {u.receiver.first_name} </div>
+                                )
+                            }
 
-
-                                    <Button onClick={e => this.randomizer(this.mapIdsForRandomizer(this.state.users))}>Randomize</Button>
-                                </div>
-                            </div>}
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
