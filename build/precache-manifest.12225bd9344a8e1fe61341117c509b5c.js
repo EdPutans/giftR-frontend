7ed4809b6a899496c0583cf27ada0bf2c7898619
Ed Putans <edputans@gmail.com>
2019-01-12 17:04:01 +0000

    build

---
+++ build/precache-manifest.12225bd9344a8e1fe61341117c509b5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "50e508b92b77d55fcddb",
+    "url": "/static/css/main.bc0667ac.chunk.css"
+  },
+  {
+    "revision": "50e508b92b77d55fcddb",
+    "url": "/static/js/main.50e508b9.chunk.js"
+  },
+  {
+    "revision": "8b70ec12f957dc78e7d2",
+    "url": "/static/css/1.4e675f63.chunk.css"
+  },
+  {
+    "revision": "8b70ec12f957dc78e7d2",
+    "url": "/static/js/1.8b70ec12.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d878a5b89a244c20c3ae51ff39ed7661",
+    "url": "/index.html"
+  }
+];
